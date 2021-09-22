43edcdc42cfa7b51fbd631fe5507618095ca9453
Daniel Gordon <dgordon@adobe.com>
2021-09-21 21:31:13 -0700

    updates to frontend module

--- ui.frontend/webpack.common.js
+++ ui.frontend/webpack.common.js
@@ -2,119 +2,80 @@
 
 const path                    = require('path');
 const webpack                 = require('webpack');
-const MiniCssExtractPlugin    = require('mini-css-extract-plugin');
-const TSConfigPathsPlugin     = require('tsconfig-paths-webpack-plugin');
-const CopyWebpackPlugin       = require('copy-webpack-plugin');
+const MiniCssExtractPlugin    = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin }  = require('clean-webpack-plugin');
 
-const SOURCE_ROOT = __dirname + '/src/main/webpack';
-
-const resolve = {
-    extensions: ['.js', '.ts'],
-    plugins: [new TSConfigPathsPlugin({
-        configFile: './tsconfig.json'
-    })]
-};
-
 module.exports = {
-    resolve: resolve,
-    entry: {
-        site: SOURCE_ROOT + '/site/main.ts'
-    },
-    output: {
-        filename: (chunkData) => {
-            return chunkData.chunk.name === 'dependencies' ? 'clientlib-dependencies/[name].js' : 'clientlib-site/[name].js';
-        },
-        path: path.resolve(__dirname, 'dist')
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                exclude: /node_modules/,
-                use: [
-                    {
-                        options: {
-                            eslintPath: require.resolve('eslint'),
-                        },
-                        loader: require.resolve('eslint-loader'),
-                    },
-                    {
-                        loader: 'ts-loader'
-                    },
-                    {
-                        loader: 'glob-import-loader',
-                        options: {
-                            resolve: resolve
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'eslint-loader',
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            url: false
-                        }
-                    },
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            plugins() {
-                                return [
-                                    require('autoprefixer')
-                                ];
-                            }
-                        }
-                    },
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            url: false
-                        }
-                    },
-                    {
-                        loader: 'glob-import-loader',
-                        options: {
-                            resolve: resolve
-                        }
-                    }
-                ]
+        resolve: {
+            alias: {
+                '../../theme.config$': path.join(__dirname, 'semanticui/theme.config')  
             }
-        ]
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new webpack.NoEmitOnErrorsPlugin(),
-        new MiniCssExtractPlugin({
-            filename: 'clientlib-[name]/[name].css'
-        }),
-        new CopyWebpackPlugin([
-            { from: path.resolve(__dirname, SOURCE_ROOT + '/resources'), to: './clientlib-site/' }
-        ])
-    ],
-    stats: {
-        assetsSort: 'chunks',
-        builtAt: true,
-        children: false,
-        chunkGroups: true,
-        chunkOrigins: true,
-        colors: false,
-        errors: true,
-        errorDetails: true,
-        env: true,
-        modules: false,
-        performance: true,
-        providedExports: false,
-        source: false,
-        warnings: true
-    }
+        },
+        entry: {
+            site: __dirname + '/src/index.js'
+        },
+        output: {
+            filename: 'semanticui-theme/js/[name].bundle.js',
+            path: path.resolve(__dirname, 'dist')
+        },
+        optimization: {
+            splitChunks: {
+                   chunks: 'all'
+                 }
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.less$/,
+                    use: [
+                      {
+                        loader: MiniCssExtractPlugin.loader
+                      },
+                      'css-loader',
+                      'less-loader'
+                    ]
+                },
+                 // this rule handles images
+                {
+                    test: /\.jpe?g$|\.gif$|\.ico$|\.png$|\.svg$/,
+                    use: 'file-loader?name=../resources/images/[name].[ext]?[hash]'
+                },
+                // the following 3 rules handle font extraction
+                {
+                    test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                    loader: 'file-loader?name=../resources/fonts/[name].[ext]&mimetype=application/font-woff'
+                },
+                {
+                    test: /\.(ttf|eot)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                    loader: 'file-loader?name=../resources/fonts/[name].[ext]'
+                },
+                {
+                test: /\.otf(\?.*)?$/,
+                use: 'file-loader?name=../resources/fonts/[name].[ext]&mimetype=application/font-otf'
+                }
+            ]
+        },
+        plugins: [
+            new CleanWebpackPlugin(),
+            new webpack.NoEmitOnErrorsPlugin(),
+            new MiniCssExtractPlugin({
+                filename: 'semanticui-theme/css/[name].bundle.css'
+            })
+        ],
+        stats: {
+            assetsSort: "chunks",
+            builtAt: true,
+            children: false,
+            chunkGroups: true,
+            chunkOrigins: true,
+            colors: false,
+            errors: true,
+            errorDetails: true,
+            env: true,
+            modules: false,
+            performance: true,
+            providedExports: false,
+            source: false,
+            warnings: true
+        }
 };
